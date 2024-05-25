66934b7d7b9c916ea941552f5b74a993e4befd04
Alinia Andersson <alinia93@gmail.com>
2024-05-25 13:02:45 +0200

    Made all pages

---
+++ src/components/UsedOnManyPages/MyNavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Nav,Container } from "react-bootstrap";
+
+function MyNavBar () {
+    return ( 
+    <>
+    
+<NavBar expand="lg">
+    <Container>
+
+        <Nav>
+
+            <Nav.Link>Meny</Nav.Link>
+        </Nav>
+    </Container>
+
+
+</NavBar>
+
+
+    </> );
+}
+
+export default ;

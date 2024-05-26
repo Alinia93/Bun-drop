c169e52c5c72b7df3dff82647be24a2f9e32d6cb
Alinia Andersson <alinia93@gmail.com>
2024-05-26 13:51:07 +0200

    fixed all routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavBar from "./components/UsedOnManyPages/MyNavBar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Favorites from "./pages/Favorites";
+import Menu from "./pages/Menu";
+import Payment from "./pages/Payment";
+import Registration from "./pages/Registration";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div>//Syns på alla sidor</div>
+      <MyNavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/favorites" element={<Favorites />} />
+
+        <Route path="/menu" element={<Menu />} />
+
+        <Route path="/payment" element={<Payment />} />
+
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/UsedOnManyPages/MyNavBar.jsx
+++ src/components/UsedOnManyPages/MyNavBar.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Navbar, Nav,Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../../images/logo.png";
 
-function MyNavBar () {
-    return ( 
+function MyNavBar() {
+  return (
     <>
-    
-<NavBar expand="lg">
-    <Container>
-
-        <Nav>
-
-            <Nav.Link>Meny</Nav.Link>
-        </Nav>
-    </Container>
-
-
-</NavBar>
-
-
-    </> );
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Bun drop logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle></Navbar.Toggle>
+          <Nav>
+            <Nav.Link href="/menu"> Menu</Nav.Link>
+            <Nav.Link> Popular burgers</Nav.Link>
+            <Nav.Link> About us</Nav.Link>
+            <Nav.Link> Your Order</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
-export default ;
+export default MyNavBar;
